dc372bea202e5ab9800b9204d972efa5f16347f5
Manik Sharma <114878881+madavsharma@users.noreply.github.com>
2024-02-12 19:09:45 +0000

    Variables and Constants

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const  accountId =11223
+let accountEmail = "abc@google.com"
+var accountPassword = "12345"
+accountcity = "Jammu" // IT STORES VALUE BUT NOT USE THIS 
+let accountState;
+ accountState = 'KASHMIR';
+
+//accountId = 2 //not allowed
+accountEmail = "def@google.com"
+accountPassword = "77667"
+accountcity = "Pune"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountcity);
+
+/* PREFER NOT TO USE 'VAR ' BECAUSE OF ISSUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE*/
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+
+
+"Use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 + 3); //code readability should be high
+console.log("Madav")
+
+let name = 'madav'
+let age = 18
+let isLoggedIn = false 
+
+/* 
+   
+     #Primitive DataTypes#
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null  => standalone value
+// undefined
+// symbol => unique
+
+
+    #Object
+
+*/
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
