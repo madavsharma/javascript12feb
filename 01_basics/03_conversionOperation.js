96c6978482366bf2c2ec88a33716c1a230317e5c
Manik Sharma <114878881+madavsharma@users.noreply.github.com>
2024-02-13 20:26:22 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+
+/* For Number */
+
+//let score = 33
+//let score1 = "33"
+//let score1 = "33abc"
+//let score1 = null
+//let score1 = undefined
+//let score1 = true
+//let score1 = false
+//let score1 = "madav"
+
+//console.log(typeof (score));
+//console.log(typeof score1);
+
+//let valueInNum = Number(score1)
+//console.log(typeof valueInNum);
+//console.log(valueInNum);
+
+// "33" => 33
+// "33abcd" => NaN
+// true => 1; false => 0
+
+/*  For Boolean   */
+
+//let isLoggedIn = 1
+//let isLoggedIn = 0
+//let isLoggedIn = ""
+//let isLoggedIn = "madav"
+//let isLoggedIn = null
+//let isLoggedIn = undefined
+
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+//"madav" => true
+
+/* For String */
+
+let someNum = 33
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof(stringNum));
